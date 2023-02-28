911dc38871d3b989b730c8fcbb0efb01a6db6251
FlickNBA <flicknba@icloud.com>
2023-02-28 10:51:24 +0100

    First 4 functions

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const add = (a, b) => {
-    return a + b;
-}
+  return a + b;
+};
 
 const subtract = (a, b) => {
-    return a - b;
-}
+  return a - b;
+};
 
 const multiply = (a, b) => {
-    return a * b;
-}
+  return a * b;
+};
 
 const divide = (a, b) => {
-    return a / b;
-}
+  return a / b;
+};
+
+console.log(add(2, 5));
+console.log(subtract(7, 4));
+console.log(multiply(5, 4));
+console.log(divide(99, 4));
